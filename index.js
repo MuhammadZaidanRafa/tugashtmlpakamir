906d3e5335886a1fe75c58c5f39bafdc2d168821
Rafa <rafabagos75@gmail.com>
2025-04-22 11:09:39 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+alert("Hello World!");</script>
+<script type="text/javascript" src="alamat.js"></script>
+ 
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Kepala Web - Inline </title>
+</head>
+<body bgcolor="#FFFFFF">
+<p id="contoh1">Ini adalah contoh tag P tanpa diformat menggunakan CSS </p>
+<p id="contoh2" style="font-size:20pt">Tag P ini diformat dengan besar font 20 point </p>
+<p id="contoh3" style="font-size:14pt; color:red">Tag P ini diformat dengan besar font 14 point, dan menggunakan warna merah </p>
+</body>
+</html>
